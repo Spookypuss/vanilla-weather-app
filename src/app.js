5afad97ad1dc5ceb3b2e3b09ba00f0fc3b2166e1
Spookypuss <114418331+Spookypuss@users.noreply.github.com>
2023-09-21 12:54:15 +0100

    Added search bar functionality, hooked into open weather api for current conditions

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
-// Time and date functionality
+// Day and time  functionality
 let now = new Date();
-
-// can format the date by putting into function and calling the function to update display page
 let date = now.getDate();
 let hours = String(now.getHours()).padStart(2, 0);
 let minutes = String(now.getMinutes()).padStart(2, 0);
@@ -36,19 +34,33 @@ let day = days[now.getDay()];
 let currentTime = document.querySelector("#current-time");
 currentTime.innerHTML = `${day}, ${hours}:${minutes}`;
 
-function searchLocation(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-input-text"); // assign variable to input search text
-  let heading = document.querySelector("h1");
-  console.log(searchInput.value);
+// hooking in to api
+function displayCurrent(response) {
+  //displays current weather passed from searchCity, below
+  let location = document.querySelector("#location-heading");
+  location.innerHTML = response.data.name; // sets name of location
+  let temperature = Math.round(response.data.main.temp); // gets current temperature and rounds to integer
+  let currentTemperature = document.querySelector("#current-temperature");
+  currentTemperature.innerHTML = temperature; // displays current temp on page
+  let description = document.querySelector("#current-description");
+  description.innerHTML = response.data.weather[0].main; // gets text description of current weather and displays on page - can maybe try description instead of main for fuller desc?
+  let humidity = document.querySelector("#humidity"); //
+  humidity.innerHTML = response.data.main.humidity; // gets humidity data and displays on page
+}
 
-  if (searchInput.value) {
-    heading.innerHTML = searchInput.value;
-  } else {
-    alert("Please enter a location");
-    heading.innerHTML = null;
-  }
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input-text");
+
+  let cityName = searchInput.value;
+  cityName = cityName.trim();
+  cityName = cityName.toLowerCase();
+
+  let apiKey = "28966f9a5b2543fb60e8a809ec2c1fd9";
+  let endpoint = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+  axios.get(endpoint).then(displayCurrent);
 }
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", searchLocation);
+searchForm.addEventListener("submit", searchCity);
